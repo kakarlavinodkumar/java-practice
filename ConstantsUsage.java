900ec06a8be133f53a92e653aee3ceebefdb7987
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-04-11 21:32:12 -0700

    Add ConstantsUsage class to demonstrate usage of constants from ConstantsExample

---
+++ ConstantsUsage.java
@@ -0,0 +1,8 @@
+public class ConstantsUsage {
+    public static void main(String[] args) {
+        // Accessing constants from ConstantsExample class
+        System.out.println("Application Name: " + ConstantsExample.APP_NAME);
+        System.out.println("Version: " + ConstantsExample.VERSION);
+        System.out.println("Value of PI: " + ConstantsExample.PI);
+    }
+}
