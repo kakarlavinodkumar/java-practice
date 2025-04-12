c7751739aa1f3aa97addc7861f9db3393170bb61
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-04-11 21:32:06 -0700

    Add ConstantsExample class to define application constants

---
+++ ConstantsExample.java
@@ -0,0 +1,6 @@
+public class ConstantsExample {
+    // Defining constants
+    public static final String APP_NAME = "Java Practice App";
+    public static final int VERSION = 1;
+    public static final double PI = 3.14159;
+}
