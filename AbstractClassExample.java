77ceafab430dda67f0ace30b38adcc6b09c1e3cd
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-04-11 21:24:40 -0700

    Add AbstractClassExample with abstract and concrete methods

---
+++ AbstractClassExample.java
@@ -0,0 +1,9 @@
+public abstract class AbstractClassExample {
+    // Abstract method (no implementation)
+    public abstract void abstractMethod();
+
+    // Concrete method (with implementation)
+    public void concreteMethod() {
+        System.out.println("This is a concrete method in the abstract class.");
+    }
+}
